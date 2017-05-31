ae4bb2e2d6202369e58d2d92db3b8be44e65444c
Mario77x <mariosavi@hotmail.com>
2017-05-31 21:34:22 +0200

    created repos page and a functional (basic) AJAX/JSON get to pull the repositories using the GitHub API

--- script/script.js
+++ script/script.js
@@ -1,21 +1,23 @@
 
   $(document).ready(function() {
-    
+
     $.ajax({
+
       type: "GET",
       dataType: "json",
-      url: "https://",
-      data: {
-        q: "",
-        type: "",
-        filter: {
-        "posts_per_page": 5
-        }
-      },
+      url: "https://api.github.com/users/Mario77x/repos",
+      token: "30beaca9fd7428c4fcbdca14c46aa8d646219edf",
+//       data: {
+// name,
+// url,
+// html_url
+// description
+
+//       },
 
       success: function(response) {
         // $("#repos").html("");
         console.log(response);
-      }
+      },
     });
   }); 
