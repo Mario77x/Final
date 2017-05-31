eef2f83f864a035c1483fde44b5aafa13e615109
Mario77x <mariosavi@hotmail.com>
2017-06-01 00:59:02 +0200

    fully functional 'repos' page + functional css and JS - css needs some extra work

--- script/script.js
+++ script/script.js
@@ -1,23 +1,24 @@
 
-  $(document).ready(function() {
+$(document).ready(function() {
 
     $.ajax({
 
-      type: "GET",
-      dataType: "json",
-      url: "https://api.github.com/users/Mario77x/repos",
-      token: "30beaca9fd7428c4fcbdca14c46aa8d646219edf",
-//       data: {
-// name,
-// url,
-// html_url
-// description
+        type: "GET",
+        dataType: "json",
+        url: "https://api.github.com/users/Mario77x/repos",
 
-//       },
 
-      success: function(response) {
-        // $("#repos").html("");
-        console.log(response);
-      },
+        success: function(response) {
+
+            for (var i=0; i<response.length; i++) {
+              
+              $("#name" + i.toString()).html("<a href='" + response[i].url + "'>" + response[i].name + "</a>");
+              $("#descrip" + i.toString()).html(response[i].description);      
+              
+            }
+        
+        }
+
     });
-  }); 
+
+}); 
