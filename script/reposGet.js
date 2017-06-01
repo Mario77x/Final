37c099637d6a94f9fc0182efec98979b67db5325
Mario77x <mariosavi@hotmail.com>
2017-06-01 12:29:38 +0200

    finalised CSS and improved AJAX/JSON get from GitHub - need to add paging option

--- script/reposGet.js
+++ script/reposGet.js
@@ -7,13 +7,12 @@ $(document).ready(function() {
         dataType: "json",
         url: "https://api.github.com/users/Mario77x/repos",
 
-
         success: function(response) {
 
             for (var i=0; i<response.length; i++) {
               
-              $("#name" + i.toString()).html("<a href='" + response[i].url + "'>" + response[i].name + "</a>");
-              $("#descrip" + i.toString()).html(response[i].description);      
+              $("#name" + i.toString()).html("<a href='" + response[i].html_url + "' target='_blank'>" + response[i].name + "</a>");
+              $("#descrip" + i.toString()).html(response[i].description);   
               
             }
         
